d8c50b8a572626f86dea6efb3b19e66c297b8d39
WNortier <warwick.nortier@gmail.com>
2020-12-22 17:32:13 +0200

    worked on event listeners and video functions

--- script.js
+++ script.js
@@ -1,21 +1,64 @@
+// Toggle play/pause icon
+const showPlayIcon = () => {
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("fa-pause", "fa-play");
+};
 
+// Play
+const togglePlay = () => {
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace("fa-play", "fa-pause");
+    playBtn.setAttribute("fa-play", "fa-pause");
+  } else {
+    video.pause();
+    showPlayIcon();
+  }
+};
 
-// Play & Pause ----------------------------------- //
+// Calculate display time format
+const displayTime = (time) => {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+};
 
+// Update progress bar as video plays
+const updateProgress = (e) => {
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`;
+};
 
+// Seek
 
-// Progress Bar ---------------------------------- //
+const setProgress = (e) => {
+  const newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+};
 
+// Volume
+const changeVolume = (e) => {
+  let volume = e.offsetX / volumeRange.offsetWidth;
+  // Rounding volume up or down
+  if (volume < 0.1) {
+    volume = 0;
+  }
+  if (volume > 0.9) {
+    volume = 1;
+  }
+  volumeBar.style.width = `${volume * 100}%`;
+  video.volume = volume;
+};
 
-
-// Volume Controls --------------------------- //
-
-
-
-// Change Playback Speed -------------------- //
-
-
-
-// Fullscreen ------------------------------- //
-
-
+// Event Listeners
+// On video end show play button icon
+video.addEventListener("ended", showPlayIcon);
+playBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+video.addEventListener("timeupdate", updateProgress);
+video.addEventListener("canplay", updateProgress);
+progressRange.addEventListener("click", setProgress);
+volumeRange.addEventListener("click", changeVolume);
